dbcdf931bcfc9d3461fdec15f1810c4057b96324
PhucDev-IT <120349317+PhucDev-IT@users.noreply.github.com>
2026-09-17 20:53:37 +0700

    feat(demo): add advanced demo entry

---
+++ apps/demo/src/advanced-main.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AdvancedDemo from './AdvancedDemo';
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AdvancedDemo/></React.StrictMode>);
