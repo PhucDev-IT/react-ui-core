1e1ed83c70a199e3d6f6075618ef045fa5e1e64b
PhucDev-IT <120349317+PhucDev-IT@users.noreply.github.com>
2026-09-17 21:08:01 +0700

    feat(demo): persist selected theme for standalone pages

--- apps/demo/src/main.tsx
+++ apps/demo/src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ChatMenuPortal from './ChatMenuPortal';
+import {DemoThemePersistence} from './DemoThemeSync';
 import './demo.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App/>
     <ChatMenuPortal/>
+    <DemoThemePersistence/>
   </React.StrictMode>,
 );
