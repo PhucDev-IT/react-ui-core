0b99423bf54a899a02c31a60347efb7245671958
PhucDev-IT <120349317+PhucDev-IT@users.noreply.github.com>
2026-09-17 21:08:16 +0700

    feat(demo): add shared theme controls to chat showcase

--- apps/demo/src/chat-main.tsx
+++ apps/demo/src/chat-main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ChatDemo from './ChatDemo';
+import {StandaloneThemeControl} from './DemoThemeSync';
 import './demo.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChatDemo />
+    <StandaloneThemeControl page="chat"/>
   </React.StrictMode>,
 );
